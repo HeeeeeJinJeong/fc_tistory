a3df3c9d01a8e79c8a4fa051bcb3fd455e1837fb
HeeeeeJinJeong <misakiyoshikuni@gmail.com>
2019-08-28 18:38:45 +0900

    좋아요 기능

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'title']
 
 admin.site.register(Post, PostAdmin)
+
+admin.site.register(Comment)
--- blogs/models.py
+++ blogs/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from users.models import User
 from helpers.models import BaseModel
+# from taggit.managers import TaggableManager
 
 
 class Post(BaseModel):
@@ -10,6 +11,20 @@ class Post(BaseModel):
     title = models.CharField(max_length=50)
     content = models.TextField()
     image = models.ImageField(blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    # tags = TaggableManager()
 
     def __str__(self):
         return '%s - %s' % (self.user, self.title)
+
+    def total_likes(self):
+        return self.likes.count()
+
+
+class Comment(BaseModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return '%s - %s' % (self.id, self.user)
